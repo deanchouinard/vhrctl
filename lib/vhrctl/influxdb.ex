7c1d04c357e6d4efd90ca273131884dc7b6dba37
Dean Chouinard <deanchouinard@gmail.com>
2022-01-29 10:26:24 -0500

    Move InfluxDB code out of controller so can use from LiveBook.

--- lib/vhrctl/influxdb.ex
+++ lib/vhrctl/influxdb.ex
@@ -2,8 +2,35 @@ defmodule VhrCtl.InfluxDB do
 @moduledoc """
 Interface to InfluxDB.
 """
-  @influxdb_url "http://localhost:8086/"
+  alias VhrCtlWeb.HTMLTable
+  #@influxdb_url "http://localhost:8086/"
+  @influxdb_url   Application.get_env(:vhrctl, :influxdb_url)
 
+  def read_table() do
+
+    %HTTPoison.Response{ body: response} = HTTPoison.get! '#{@influxdb_url}/query?db=mydb;q=select+*+from+env+order+by+desc+limit+20;'
+
+    response = Poison.decode(response)
+    #{:ok, "results" => [%{"series" => [%{"columns" => col}]}]} = response
+    IO.inspect response, label: "RESPONSE"
+    {:ok, %{"results" => [%{"series" => [%{"columns" => c, "values" => v}]}]}} = response
+
+    columns = ["time","batt","humid", "temp"]
+
+    t_str = "<table><thead>"
+    t_header = Enum.reduce(columns, t_str, &(HTMLTable.header(&1, &2)))
+    t_str = t_header <> "</thead><tbody>"
+
+    tabl = Enum.reduce(v, t_str, &(HTMLTable.detail(&1,&2)))
+    tabl = tabl <> "</tbody></table"
+
+    IO.puts tabl
+    IO.inspect c, label: "COL"
+    IO.inspect v, label: "VAL"
+    IO.inspect response, label: "RESPONSE"
+
+    tabl
+  end
   def insert(temp, humid, batt) do
 
     data = 'env,location=unit43,sensor=test temp=#{temp},humid=#{humid},batt=#{batt}'
--- lib/vhrctl_web/controllers/ui_controller.ex
+++ lib/vhrctl_web/controllers/ui_controller.ex
@@ -1,6 +1,6 @@
 defmodule VhrCtlWeb.UiController do
   use VhrCtlWeb, :controller
-  alias VhrCtlWeb.HTMLTable
+
 
   @influxdb_url   Application.get_env(:vhrctl, :influxdb_url)
   @robot_url   Application.get_env(:vhrctl, :robot_url)
@@ -67,26 +67,7 @@ defmodule VhrCtlWeb.UiController do
   end
 
   def iflx(conn, _params) do
-    %HTTPoison.Response{ body: response} =
-      HTTPoison.get! '#{@influxdb_url}/query?db=mydb;q=select+*+from+env+order+by+desc+limit+20;'
-
-    response = Poison.decode(response)
-    #{:ok, "results" => [%{"series" => [%{"columns" => col}]}]} = response
-    {:ok, %{"results" => [%{"series" => [%{"columns" => c, "values" => v}]}]}} = response
-
-    columns = ["time","batt","humid", "temp"]
-
-    t_str = "<table><thead>"
-    t_header = Enum.reduce(columns, t_str, &(HTMLTable.header(&1, &2)))
-    t_str = t_header <> "</thead><tbody>"
-
-    tabl = Enum.reduce(v, t_str, &(HTMLTable.detail(&1,&2)))
-    tabl = tabl <> "</tbody></table"
-
-    IO.puts tabl
-    IO.inspect c, label: "COL"
-    IO.inspect v, label: "VAL"
-    IO.inspect response, label: "RESPONSE"
+    tabl = VhrCtl.InfluxDB.read_table()
     render(conn, "index.html", messenger: tabl)
   end
 
